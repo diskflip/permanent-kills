adfe30486730364cc0a005cbba4342070967aed6
diskflip <jmwilliams0604@gmail.com>
2025-11-09 01:14:45 -0700

    refactored and set up build

--- src/test/java/com/permanentdeath/PermanentDeathTest.java
+++ src/test/java/com/permanentdeath/PermanentDeathTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.permanentdeath;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PermanentDeathTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PermanentDeathPlugin.class);
 		RuneLite.main(args);
 	}
 }
